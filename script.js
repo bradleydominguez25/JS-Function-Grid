ab25b7a08fdfdc63447806d666c13d0cd5667caf
Bradley Dominguez <bradleydominguez25@gmail.com>
2017-02-27 13:20:58 +0000

    You made changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//Write your function calls below this line
+setColor(0, 0, "blue");
+setColor(0, 9, "blue");
+setColor(9, 0, "blue");
+setColor(9, 9, "blue");
+setColor(2, 6, "red");
+setColor(2, 5, "red");
+setColor(2, 4, "red");
+setColor(2, 3, "red");
+setColor(3, 4, "red");
+
+
+
